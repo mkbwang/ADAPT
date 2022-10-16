aa388c9c0572fda3cd076ee48019a158123cc0df
Mukai Wang <wangmk@umich.edu>
2022-10-16 18:59:48 -0400

    Find out the bug that is causing the result on biostat cluster different from that of the local computer.

--- simulation/ANCOM_trial.R
+++ simulation/ANCOM_trial.R
@@ -1,16 +1,18 @@
 rm(list=ls())
 folder = '/home/wangmk/UM/Research/MDAWG/DiffRatio/simulation'
+# folder = '.'
+fnum <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 library(dplyr)
+input_fname <- sprintf('simulated_data_%d.rds', fnum)
+data <- readRDS(file.path(folder, 'data', input_fname))
 
-data <- readRDS(file.path(folder, 'simulated_data.rds'))
 abn_info <- data$mean.eco.abn
 
 taxa_names <- row.names(abn_info)
 taxa_pairs <- combn(taxa_names, 2) %>% t() %>% as.data.frame()
 colnames(taxa_pairs) <- c("T1", "T2")
-
-
 counts <- data$obs.abn
+
 indicator <- data$grp - 1
 
 # try ANCOM
@@ -26,11 +28,11 @@ registerDoParallel(cl)
 
 
 ptm <- proc.time()
-outcome <- foreach(j=1:nrow(ancom_result), .combine=rbind,
+outcome <- foreach(j=1:700, .combine=rbind,
                    .packages=c('dplyr'), .inorder=FALSE,
                    .errorhandling="remove") %dopar% {
-  t1 <- ancom_result$T1[j]
-  t2 <- ancom_result$T2[j]
+  t1 <- as.character(ancom_result$T1[j])
+  t2 <- as.character(ancom_result$T2[j])
   selected_counts <- counts[c(t1, t2),] %>%
     t() %>% as.data.frame()
   colnames(selected_counts) <- c('T1', 'T2')
@@ -41,7 +43,12 @@ outcome <- foreach(j=1:nrow(ancom_result), .combine=rbind,
 }
 duration <- proc.time() - ptm
 
-ancom_result$pval <- outcome[, 2]
+ancom_result$pval[outcome[, 1]] <- outcome[, 2]
 
-saveRDS(ancom_result, file=file.path(folder, 'ancom_result.rds'))
+filename <- sprintf('ancom_result_%d.csv', fnum)
+write.csv(ancom_result, file.path(folder, 'result', filename),
+          row.names = FALSE)
+
+
+
 
