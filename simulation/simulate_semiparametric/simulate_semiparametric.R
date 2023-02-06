53f7c2172d56ee714070185663e86f2350ba7a6f
Mukai Wang <wangmk@umich.edu>
2023-02-06 00:04:10 -0500

    Decide on a (simple) simulation scheme to generate data with no DA taxa.

--- simulation/simulate_semiparametric/simulate_semiparametric.R
+++ simulation/simulate_semiparametric/simulate_semiparametric.R
@@ -1,9 +1,12 @@
 library(ExperimentHub)
 library(HMP16SData)
 library(GUniFrac)
+library(dirmult)
 library(phyloseq)
 library(dplyr)
+library(MASS)
 
+remove(list=ls())
 
 summary_stats <- V35() %>% table_one()
 colnames(summary_stats) <- c("Visit_Number", "Sex", "Center",
@@ -38,6 +41,43 @@ colnames(template_data) <- sprintf('Indv%d', seq(1, 191))
 
 folder <- '/home/wangmk/UM/Research/MDAWG/DiffRatio/simulation/data/semiparametric'
 
+
+# simulate null data
+## estimate dirichlet multinomial distribution
+rdirichlet.m <- function (alpha) {
+  Gam <- rgamma(length(alpha), shape = alpha)
+  Gam / sum(Gam)
+}
+
+
+simMSeq_null <- function(nSam=400, nTaxa=300, depth.mu=10000, depth.theta=100, seed=1){
+  # simulate count matrix with no DA taxa
+  set.seed(seed+2020)
+  nSeq <- rnegbin(nSam, mu = depth.mu , theta = depth.theta) # generating sequencing depths
+  # generate count matrices from multinomial distributions
+  otu.tab.sim <- sapply(1:nSam, function (i) rmultinom(1, nSeq[i], rdirichlet.m(alpha = rep(0.5, nTaxa))))
+  covariates <- rep(0, nSam)
+  covariates[sample(nSam, nSam/2)] <- 1
+  # filter out taxa that have too many zeros in either group
+  group0 <- otu.tab.sim[, covariates == 0]
+  group1 <- otu.tab.sim[, covariates == 1]
+  admissible_taxa <- (rowMeans(group0 == 0) < 0.8) & (rowMeans(group1 == 0) < 0.8)
+  otu.tab.sim <- otu.tab.sim[admissible_taxa, ]
+  row.names(otu.tab.sim) <- sprintf("Taxon%d", seq(1, sum(admissible_taxa)))
+  return(list(otu.tab.sim=otu.tab.sim,  covariates=covariates))
+}
+
+
+
+for (j in 1:100){
+  simulated_data <- simMSeq_null(seed=j)
+  output_file <- sprintf('simulated_data_null_%d.rds', j)
+  saveRDS(simulated_data, file.path(folder, output_file))
+}
+
+
+
+# simulate datasets with DAs
 for (j in 1:100){
   print(j)
   set.seed(j)
