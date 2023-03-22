14172b37f8413ceb782586f49b3a90f5dbf37ab2
Mukai Wang <wangmk@umich.edu>
2023-03-22 17:44:32 -0400

    Edit the code for presentation.

--- simulation/DiffTaxon_ID/Generalized_Least_Squares.R
+++ simulation/DiffTaxon_ID/Generalized_Least_Squares.R
@@ -1,74 +1,111 @@
+## ----setup, include=FALSE----------------------------------------------------------
 rm(list=ls())
-# load data
+library(Matrix)
+library(ggplot2)
+library(DESeq2)
+library(cowplot)
 
-data_withDA <- readRDS(file.path('simulation', 'data', 'PoiGamma', 'simple_simulation_withDA2.rds'))
-truth <- data_withDA$mean.eco.abn
-glm_result <- read.csv(file.path('simulation', 'glmdisp_result', 'glmdisp_simple_result_withDA2.csv'))
+
+## ----------------------------------------------------------------------------------
+folder <- "/home/wangmk/UM/Research/MDAWG/DiffRatio/simulation"
+data <- readRDS(file.path(folder, 'data', 'PoiGamma', 
+                          'simple_simulation_withDA2.rds'))
+truth <- data$mean.eco.abn
+num_taxa <- nrow(truth) # 100 taxa
+glm_result <- read.csv(file.path(folder, 'glmdisp_result', 
+                                 'glmdisp_simple_result_withDA2.csv'))
 
 
+## ----------------------------------------------------------------------------------
 Z_score <- glm_result$effect / glm_result$SE
-Z_score <- c(0, Z_score) # assume that we have observed theta_1 to be zero
-num_taxa <- 100
+Z_score <- c(0, Z_score)
+cat(sprintf("The number of response variables are %d", length(Z_score)))
+
+
+## ----------------------------------------------------------------------------------
+# one row for each taxa pair
 allpairs <- combn(seq(1, num_taxa), m=2)
-design_matrix <- matrix(0, nrow=nrow(glm_result), ncol=100)
+design_matrix <- matrix(0, nrow=nrow(glm_result), ncol=num_taxa)
 for (j in 1:ncol(allpairs)){
   design_matrix[j, allpairs[1, j]] <- 1
   design_matrix[j, allpairs[2, j]] <- -1
 }
+initial_rank <- rankMatrix(design_matrix)[[1]]
+cat(sprintf("Initial design matrix has a rank of %d. \n", initial_rank))
+
+# one extra row for theta_1
 design_matrix <- rbind(c(1, rep(0, num_taxa - 1)), design_matrix)
+final_rank <- rankMatrix(design_matrix)[[1]]
+cat(sprintf("The final design matrix has a rank of %d.", final_rank))
 
-# assume that all the thetas have the same variance sigma^2 and all the z scores given
-# thetas have variance 1/4*sigma^2
 
+## ----------------------------------------------------------------------------------
 V_mat <- design_matrix %*% t(design_matrix)
-V_mat <- V_mat + diag(1/4, nrow=4951, ncol=4951)
-V_mat[1,1] <- 1
-
-# this inverse takes a long time
-# need to improve (e.g. only fit a subset of logistic regressions)
-ptm <- proc.time()
-V_inv <- chol2inv(chol(V_mat))
-proc.time() - ptm
+V_mat <- V_mat + diag(1/4, nrow=nrow(V_mat), ncol=ncol(V_mat))
+V_mat[1,1] <- 1 # the top left entry doesn't have epsilon^2
 
 
+## ----------------------------------------------------------------------------------
+if (file.exists(file.path(folder, 'DiffTaxon_ID', 'example_V_inv.rds'))){
+  V_inv <- readRDS(file.path(folder, 'DiffTaxon_ID', 'example_V_inv.rds'))
+} else{
+  V_inv <- chol2inv(chol(V_mat))
+}
+
+
+## ----------------------------------------------------------------------------------
 XtVX <- t(design_matrix) %*% V_inv %*% design_matrix
 XtVX_inv <- chol2inv(chol(XtVX))
 
-# generalized least square estimate
+
+## ----------------------------------------------------------------------------------
+# estimate tau
 tau_hat <- XtVX_inv %*% t(design_matrix) %*% V_inv %*% Z_score
 # residual
 resid <- Z_score - design_matrix %*% tau_hat
 # sigma^2 estimate (MLE)
 sigma2_hat <- t(resid) %*% V_inv %*% resid / nrow(resid)
+# estimated covariance of tau
 covar_tau <- drop(sigma2_hat) * XtVX_inv
 stderror_tau <- sqrt(diag(covar_tau))
 
-# visualize the distribution of estimated taus for each taxon
-library(ggplot2)
-strength <- data.frame(estimate = tau_hat)
-ggplot(strength, aes(x=estimate)) +
+
+## ---- fig.width=7, fig.height=3----------------------------------------------------
+strength <- data.frame(tau_hat = tau_hat)
+strength$logfoldchange <- -log(truth$effect.size)
+
+histogram_plot <- ggplot(strength, aes(x=tau_hat)) +
   geom_histogram(color="black", fill="white", binwidth=1)+
-  xlab("Estimated Strength Correction")
+  geom_vline(xintercept=0, linetype="dashed", color = "red") +
+  xlab("Estimated Tau") + ylab("Frequency") + theme_bw()
+
+scatter_plot <- ggplot(strength, aes(x=tau_hat, y=logfoldchange)) +
+  geom_point(size=0.8, alpha=0.8) + 
+  geom_vline(xintercept=0, linetype="dashed", color = "red") +
+  geom_hline(yintercept=0, linetype="dashed", color = "red") +
+  xlab("Estimated Tau") + 
+  ylab("Log Fold Change of Absolute Abundance") + 
+  theme_bw()
+
+plot_grid(histogram_plot, scatter_plot)
 
 
-# we notice that the mode(peak of density) isn't at zero. This is because the selected theta_1 isn't actually zero.
-# No matter, we can shift all the tau estimates
+## ----------------------------------------------------------------------------------
 fitted_density <- density(tau_hat)
 shift <- fitted_density$x[which.max(fitted_density$y)]
-
 shifted_tau_hat <- tau_hat - shift
 
-# hypothesis tests for each tau(whether they were significantly different from zero)
+
+## ----------------------------------------------------------------------------------
 test_statistic <- as.vector(shifted_tau_hat / stderror_tau)
-pval <- 1 - pnorm(abs(test_statistic))
+pval <- pnorm(-abs(test_statistic))
+performance <- truth
+performance$prediction <- pval < 0.05
+contingency_table <- table(performance$prediction, truth$effect.size !=1)
 
 
-prediction <- pval < 0.05
-table(prediction, truth$effect.size !=1)
-
-
-## if do not consider correlation between z scores
-
+## ----------------------------------------------------------------------------------
+# OLS estimates
 XtX <- t(design_matrix) %*% design_matrix
 XtX_inv <- chol2inv(chol(XtX))
 tau_hat_nocorr <- XtX_inv %*% t(design_matrix) %*% Z_score
@@ -76,30 +113,35 @@ resid_nocorr <- Z_score - design_matrix %*% tau_hat_nocorr
 sigma2_hat_nocorr <- (t(resid_nocorr) %*% resid_nocorr) / nrow(resid_nocorr)
 covar_tau_nocorr <- drop(sigma2_hat_nocorr) * XtX_inv
 stderror_tau_nocorr <- sqrt(diag(covar_tau_nocorr))
+
+# shift
 fitted_density_nocorr <- density(tau_hat_nocorr)
 shift_nocorr <- fitted_density_nocorr$x[which.max(fitted_density_nocorr$y)]
-
 shifted_tau_hat_nocorr <- tau_hat_nocorr - shift_nocorr
 
+# test
 test_statistic_nocorr <- as.vector(shifted_tau_hat_nocorr / stderror_tau_nocorr)
-pval_nocorr <- 1 - pnorm(abs(test_statistic_nocorr))
-prediction_nocorr <- pval_nocorr < 0.05
-table(prediction_nocorr, truth$effect.size !=1)
+pval_nocorr <- pnorm(-abs(test_statistic_nocorr))
+performance$prediction_nocorr <- pval_nocorr < 0.05
+contingency_table_nocorr <- table(performance$prediction_nocorr, 
+                                  truth$effect.size !=1)
 
-## in constrast with DeSeq2
 
-library(DESeq2)
-count_data <- as.matrix(data_withDA$obs.abn) |> unname()
 
+## ---- message=FALSE----------------------------------------------------------------
+
+count_data <- as.matrix(data$obs.abn) |> unname()
 metadata <- data.frame(id = sprintf("sub%d", seq(1, 100)),
                        group = c(rep(0, 50), rep(1, 50)))
 metadata$group <- as.factor(metadata$group)
-
 dds <- DESeqDataSetFromMatrix(countData=count_data,
                               colData=metadata,
                               design=~group)
-
 dds <- DESeq(dds)
 deseq_result <- results(dds)
-deseq_prediction <- deseq_result$padj < 0.05
-table(deseq_prediction, truth$effect.size !=1)
+performance$deseq_prediction <- deseq_result$padj < 0.05
+contingency_table_deseq <- table(performance$deseq_prediction, 
+                                 truth$effect.size !=1)
+
+
+
